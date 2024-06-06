7cb1a2647eaef76e7265394e9ed9f3a2b8c8e69d
Prakash Batra <Prakash Batra@PRAKASHBATRA>
2024-06-06 09:42:39 -0400

    All classes mentioned in the assignment are declared. Position Class with properties x, y and constructor. Player class Board class Cell class Game class

---
+++ GemHunters/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace GemHunters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            
+        }
+    }
+
+    // Position class
+    class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    // Player class
+    class Player
+    {
+        
+    }
+    // Board class
+    class Board
+    {
+
+        public Board()
+        {
+
+
+
+        }
+    }
+
+    // Cell class
+    class Cell
+    {
+      
+    }
+
+    // Game class
+    class Game
+        {
+            
+
+            public Game()
+            {
+               
+            }
+        }
+    }
+
